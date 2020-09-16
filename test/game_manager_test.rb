519b6ee1b655ee0989fc243537fca6c23763560b
Brian Liu <brian.b.liu@gmail.com>
2020-09-16 17:17:47 -0600

    Clean up

--- test/game_manager_test.rb
+++ test/game_manager_test.rb
@@ -1,16 +1,14 @@
-require './test/test_helper'
-# require 'minitest/autorun'
-# require 'minitest/pride'
+require 'minitest/autorun'
+require 'minitest/pride'
 require 'mocha/minitest'
-require 'mocha/minitest'
-# require 'Pry'
+require 'Pry'
 require './lib/game'
 require './lib/game_manager'
 
 class GameManagerTest < MiniTest::Test
   def setup
     game_path = './data/games_dummy.csv'
-    @game_manager = GameManager.new(game_path, "tracker")
+    @game_manager = GameManager.new(game_path, 'tracker')
   end
 
   def test_it_exists
@@ -32,7 +30,7 @@ class GameManagerTest < MiniTest::Test
   end
 
   def test_game_manager_can_calculate_percentage_home_wins
-   assert_equal 0.53, @game_manager.percentage_home_wins
+    assert_equal 0.53, @game_manager.percentage_home_wins
   end
 
   def test_game_manager_can_calculate_percentage_visitor_wins
@@ -44,30 +42,30 @@ class GameManagerTest < MiniTest::Test
   end
 
   def test_it_can_average_goals_by_season
-    assert_equal ({'20132014' => 4.0, '20122013' => 4.36, "20162017" => 4.75}), @game_manager.average_goals_by_season
+    assert_equal ({ '20132014' => 4.0, '20122013' => 4.36, '20162017' => 4.75 }), @game_manager.average_goals_by_season
   end
 
   def test_it_finds_highest_scoring_visitor
-    @game_manager.tracker.stubs(:get_team_name).returns("FC Cincinnati")
-    assert_equal "FC Cincinnati", @game_manager.highest_scoring_visitor
+    @game_manager.tracker.stubs(:get_team_name).returns('FC Cincinnati')
+    assert_equal 'FC Cincinnati', @game_manager.highest_scoring_visitor
   end
 
   def test_game_manager_finds_highest_scoring_home_team
-    @game_manager.tracker.stubs(:get_team_name).returns("Chicago Fire")
-    assert_equal "Chicago Fire", @game_manager.highest_scoring_home_team
+    @game_manager.tracker.stubs(:get_team_name).returns('Chicago Fire')
+    assert_equal 'Chicago Fire', @game_manager.highest_scoring_home_team
   end
 
   def test_can_find_lowest_scoring_visitor
-    @game_manager.tracker.stubs(:get_team_name).returns("Atlanta United")
-    assert_equal "Atlanta United", @game_manager.lowest_scoring_visitor
+    @game_manager.tracker.stubs(:get_team_name).returns('Atlanta United')
+    assert_equal 'Atlanta United', @game_manager.lowest_scoring_visitor
   end
 
   def test_game_manager_can_get_ids_of_games_in_season
-    assert_equal ["2013020674", "2013020177"], @game_manager.get_season_game_ids("20132014")
+    assert_equal ['2013020674', '2013020177'], @game_manager.get_season_game_ids('20132014')
   end
 
   def test_game_manager_can_count_games_by_season
-    assert_equal ({"20132014"=>2, "20122013"=>11, "20162017" => 4}), @game_manager.count_of_games_by_season
+    assert_equal ({ '20132014' => 2, '20122013' => 11, '20162017' => 4 }), @game_manager.count_of_games_by_season
   end
 
   def test_game_manager_can_average_goals_per_game
@@ -75,39 +73,25 @@ class GameManagerTest < MiniTest::Test
   end
 
   def test_can_find_lowest_scoring_home_team
-    @game_manager.tracker.stubs(:get_team_name).returns("Atlanta United")
-    assert_equal "Atlanta United", @game_manager.lowest_scoring_home_team
+    @game_manager.tracker.stubs(:get_team_name).returns('Atlanta United')
+    assert_equal 'Atlanta United', @game_manager.lowest_scoring_home_team
   end
 
   def test_can_get_team_with_best_season
-    assert_equal "20132014", @game_manager.best_season("24")
+    assert_equal '20132014', @game_manager.best_season('24')
   end
 
   def test_can_get_worst_season_for_team
-    assert_equal "20122013", @game_manager.worst_season("24")
-  end
-
-  def test_it_can_show_how_many_wins_per_season
-    skip
-    wins_by_season = Hash.new(0.0),
-    games_by_season = Hash.new { |hash, key| hash[key] = [] }
-    @game_manager.wins_per_season("24", wins_by_season, games_by_season)
-    # games_by_season.each do |season, games|
-    #   games.each_do do |game|
-    #     assert_instance_of Game, game
-    #   end
-    # end
-    assert_equal ({"20132014"=>1.0, "20122013"=>1.0, "20162017"=>4.0}), wins_by_season
+    assert_equal '20122013', @game_manager.worst_season('24')
   end
 
   def test_it_can_find_winningest_coach
-    @game_manager.tracker.stubs(:find_winningest_coach).returns("John Tortorella")
-    assert_equal "John Tortorella", @game_manager.winningest_coach("20162017")
+    @game_manager.tracker.stubs(:find_winningest_coach).returns('John Tortorella')
+    assert_equal 'John Tortorella', @game_manager.winningest_coach('20162017')
   end
 
   def test_it_can_find_worst_coach
-    @game_manager.tracker.stubs(:find_winningest_coach).returns("John Tortorella")
-    assert_equal "John Tortorella", @game_manager.worst_coach("20162017")
+    @game_manager.tracker.stubs(:find_worst_coach).returns('John Tortorella')
+    assert_equal 'John Tortorella', @game_manager.worst_coach('20162017')
   end
-
 end
